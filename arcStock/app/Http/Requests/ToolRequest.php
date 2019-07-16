173577a6f1fecc977eb0e8cd15e84a2c7cb2da2b
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-07-16 15:12:22 +0200

    Prepare files for tools management

---
+++ arcStock/app/Http/Requests/ToolRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ToolRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:tools',
+            'type' => 'required|max:2',
+            'number' => 'required'
+        ];
+    }
+}
---
+++ arcStock/app/Tool.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tool extends Model
+{
+    protected $table = 'tools';
+
+    public  $timestamps = false;
+
+}
