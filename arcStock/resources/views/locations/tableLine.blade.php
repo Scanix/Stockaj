eb12f70c36c3dce6e4af336645183d768683a6c8
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-07-21 13:38:16 +0200

    Create views for a location

---
+++ arcStock/resources/views/locations/tableLine.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td>{{ $location->id }}</td>
+    <td>{{ $location->created_at }}</td>
+    <td>{{ $location->tool->name }}</td>
+    <td>{{ $location->person->name }}</td>
+    <td>{{ $location->isOver }}</td>
+</tr>
