16f8c860e8661ec4f128e7a4e6af2c7edf351e8e
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-07-21 13:39:02 +0200

    Make card wider

--- arcStock/resources/views/tools/list.blade.php
+++ arcStock/resources/views/tools/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Tools list') }}</div>
 
@@ -23,6 +23,7 @@
                     </table>
 
                     {{ $tools->appends(['order' => \Illuminate\Support\Facades\Input::get('order')])->links() }}
+                    <a href="{{ route('tools.create') }}"><button class="btn btn-block">{{ __('Add tool') }}</button></a>
                 </div>
             </div>
         </div>
