6fe078a49f36b0581e634faf4bcf4d7009675d22
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-08-01 17:27:15 +0200

    Prepare layout for the notifications menu

---
+++ arcStock/resources/views/layouts/notifications.blade.php
@@ -0,0 +1,9 @@
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        <i class="fa fa-bell"></i>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+    </div>
+</li>
